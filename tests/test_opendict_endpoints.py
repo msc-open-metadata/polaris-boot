b3139b766c20f4936353c0cefa61db9ef1cdbe2a
akongstad <kongstad@outlook.com>
2025-03-13 16:13:59 +0100

    feat: enable debugging on polaris container and provide unitest for endpoint reachability

--- tests/test_opendict_endpoints.py
+++ tests/test_opendict_endpoints.py
@@ -1,9 +1,10 @@
-import requests
-import dotenv
+import json
 import os
-import re
 
+import dotenv
+import requests
 from requests.exceptions import HTTPError
+
 
 def get_auth_token() -> str:
     dotenv.load_dotenv(".env")
@@ -16,7 +17,7 @@ def get_auth_token() -> str:
         "grant_type": "client_credentials",
         "client_id": client_id,
         "client_secret": client_secret,
-        "scope": "PRINCIPAL_ROLE:ALL"
+        "scope": "PRINCIPAL_ROLE:ALL",
     }
 
     response: requests.Response = requests.post("http://localhost:8181/api/catalog/v1/oauth/tokens", data=data)
@@ -25,33 +26,72 @@ def get_auth_token() -> str:
         token = response.json()["access_token"]
         return token
     except HTTPError as e:
-           print(f"Error: {e}")
-           print(f"Response: {response.text} ")
-           exit(1)
+        print(f"Error: {e}")
+        print(f"Response: {response.text} ")
+        exit(1)
 
-def test_create_udo():
-     token = get_auth_token()
 
-     # Prepare request data.
-     data = {
-         "name": "AndreasFunc",
-         "description": "Test UDO description",
-         "type": "TEST"
-     }
+def test_show_all_udos():
+    token = get_auth_token()
 
-     headers = {
-         "Authorization": f"Bearer {token}"
-     }
+    headers = {"Authorization": f"Bearer {token}"}
 
-     response: requests.Response = requests.post("http://localhost:8181/api/catalog/v1/udos", json=data, headers=headers)
+    response: requests.Response = requests.get("http://localhost:8181/api/opendic/v1/objects", headers=headers)
 
-     try:
-         response.raise_for_status()
-         print(f"Response: {response.text}")
-     except HTTPError as e:
-           print(f"Error: {e}")
-           print(f"Response: {response.text}")
-           exit(1)
+    try:
+        response.raise_for_status()
+        print(f"Response: {response.text}")
+    except HTTPError as e:
+        print(f"Error: {e}")
+        print(f"Response: {response.text}")
+        assert response.status_code == 501
 
-def test_show_udo():
-    return
+
+def test_show_function_udo():
+    token = get_auth_token()
+
+    headers = {"Authorization": f"Bearer {token}"}
+
+    response: requests.Response = requests.get(
+        "http://localhost:8181/api/opendic/v1/objects/function/", headers=headers
+    )
+
+    try:
+        response.raise_for_status()
+        print(f"Response: {response.text}")
+    except HTTPError as e:
+        print(f"Error: {e}")
+        print(f"Response: {response.text}")
+        assert response.status_code == 501
+
+
+def test_create_function_udo():
+    token = get_auth_token()
+
+    headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+
+    data = json.dumps({
+        "udo": {
+            "type": "function",
+            "name": "function",
+            "props": {
+                    "args": {
+                        "arg1": "string",
+                        "arg2": "number"
+                    },
+                    "language": "sql"
+                }
+        }
+    })
+
+    response: requests.Response = requests.post(
+        "http://localhost:8181/api/opendic/v1/objects/function/", headers=headers, data=data
+    )
+
+    try:
+        response.raise_for_status()
+        print(f"Response: {response.text}")
+    except HTTPError as e:
+        print(f"Error: {e}")
+        print(f"Response: {response.text}")
+        assert response.status_code == 501
