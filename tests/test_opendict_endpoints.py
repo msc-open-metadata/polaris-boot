3ba2c808edf507b150e146c9194f8d32e40d5b7c
akongstad <kongstad@outlook.com>
2025-04-02 18:54:28 +0200

    feat: create e2e test suite

--- tests/test_opendict_endpoints.py
+++ tests/test_opendict_endpoints.py
@@ -5,6 +5,10 @@ import dotenv
 import requests
 from requests.exceptions import HTTPError
 
+"""
+Polaris opendic end-2-end tests.
+"""
+
 
 def get_auth_token() -> str:
     dotenv.load_dotenv(".env")
@@ -23,52 +27,19 @@ def get_auth_token() -> str:
     response: requests.Response = requests.post("http://localhost:8181/api/catalog/v1/oauth/tokens", data=data)
 
     try:
-        token = response.json()["access_token"]
-        return token
+        TOKEN = response.json()["access_token"]
+        return TOKEN
     except HTTPError as e:
         print(f"Error: {e}")
         print(f"Response: {response.text} ")
         exit(1)
 
 
-def test_show_all_udos():
-    token = get_auth_token()
-
-    headers = {"Authorization": f"Bearer {token}"}
-
-    response: requests.Response = requests.get("http://localhost:8181/api/opendic/v1/objects", headers=headers)
-
-    try:
-        response.raise_for_status()
-        print(f"Response: {response.json()}")
-    except HTTPError as e:
-        print(f"Error: {e}")
-        print(f"Response: {response.json()}")
+TOKEN = get_auth_token()
 
 
-def test_show_function_udo():
-    token = get_auth_token()
-
-    headers = {"Authorization": f"Bearer {token}"}
-
-    response: requests.Response = requests.get(
-        "http://localhost:8181/api/opendic/v1/objects/function/", headers=headers
-    )
-
-    try:
-        response.raise_for_status()
-        print(f"Response: {response.json()}")
-    except HTTPError as e:
-        print(f"Error: {e}")
-        print(f"Response: {response.json()}")
-
-    assert response.status_code == 200
-
-
-def test_define_function_udo():
-    token = get_auth_token()
-
-    headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+def test_001_define_function_udo():
+    headers = {"Authorization": f"Bearer {TOKEN}", "Content-Type": "application/json"}
 
     data = json.dumps(
         {
@@ -97,13 +68,50 @@ def test_define_function_udo():
     except HTTPError as e:
         print(f"Error: {e}")
         print(f"Response: {response.json()}")
-        assert response.status_code in {501}
+
+    assert response.status_code in {201, 409}
+    if response.status_code == 409:
+        assert "Type function already exists with schema" in response.json()["error"]["message"]
 
 
-def test_create_function_udo():
-    token = get_auth_token()
+def test_002_show_all_udos():
+    headers = {"Authorization": f"Bearer {TOKEN}"}
 
-    headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+    response: requests.Response = requests.get("http://localhost:8181/api/opendic/v1/objects", headers=headers)
+
+    try:
+        response.raise_for_status()
+        print(f"Response: {response.json()}")
+    except HTTPError as e:
+        print(f"Error: {e}")
+        print(f"Response: {response.json()}")
+
+    assert response.status_code == 200
+    assert "function" in response.json()
+
+
+def test_003_show_function_udo():
+    headers = {"Authorization": f"Bearer {TOKEN}"}
+
+    response: requests.Response = requests.get(
+        "http://localhost:8181/api/opendic/v1/objects/function/", headers=headers
+    )
+
+    try:
+        response.raise_for_status()
+        print(f"Response: {response.json()}")
+
+    except HTTPError as e:
+        print(f"Error: {e}")
+        print(f"Response: {response.json()}")
+
+    assert isinstance(response.json(), list)
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+
+def test_004_create_function_udo():
+    headers = {"Authorization": f"Bearer {TOKEN}", "Content-Type": "application/json"}
 
     data = json.dumps(
         {
@@ -118,8 +126,7 @@ def test_create_function_udo():
                     "runtime": "3.12",
                     "client_version": "1",
                     "return_type": "number",
-                    "signature": "foo(arg1: str, arg2: int) -> str"
-
+                    "signature": "foo(arg1: str, arg2: int) -> str",
                 },
             }
         }
@@ -136,52 +143,29 @@ def test_create_function_udo():
     except HTTPError as e:
         print(f"Error: {e}")
         print(f"Response: {response.json()}")
-        assert response.status_code in {501, 409}
+
+    assert response.status_code in {201}
 
 
-def test_drop_function_udo():
-    token = get_auth_token()
+def test_005_add_platform_mapping():
+    TOKEN = get_auth_token()
 
-    headers = {"Authorization": f"Bearer {token}"}
+    headers = {"Authorization": f"Bearer {TOKEN}", "Content-Type": "application/json"}
 
-    response: requests.Response = requests.delete(
-        "http://localhost:8181/api/opendic/v1/objects/platform_mappings", headers=headers
-    )
-
-    try:
-        response.raise_for_status()
-        print(f"Status: {response.status_code}")
-        print(f"Response: {response.json()}")
-    except HTTPError as e:
-        print(f"Error: {e}")
-        print(f"Response: {response.json()}")
-        assert response.status_code in {501, 409}
-
-
-def test_add_platform_mapping():
-    token = get_auth_token()
-
-    headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
-
-    # data1 = json.dumps(
-    #     {
-    #         "createplatformmappingrequest": {
-    #             "typeName": "and_func",
-    #             "platformName": "snowflake",
-    #             "syntax": "CREATE OR ALTER <type> <function_name>(<args>)\n    RETURNS <return_type>\n    LANGUAGE <language>\n    RUNTIME = <runtime>\n    PACKAGES = (<packages>)\n    HANDLER = '<name>'\n    AS $$\n<def>\n$$",
-    #             "objectDumpMap": {
-    #                 "args": {"type": "map", "format": "<key> <value>", "delimiter": ", "},
-    #                 "packages": {"type": "list", "format": "'<item>'", "delimiter": ", "},
-    #             },
-    #         }
-    #     }
-    # )
     data = json.dumps(
         {
             "platformMapping": {
-                "typeName": "function",
+                "typeName": "and_func",
                 "platformName": "snowflake",
                 "syntax": "CREATE OR ALTER <type> <signature>\n    RETURNS <return_type>\n    LANGUAGE <language>\n    RUNTIME = <runtime>\n    HANDLER = '<name>'\n    AS $$\n<def>\n$$",
+                "objectDumpMap": {
+                    "args": {
+                        "propType": "map",
+                        "format": "<key> <value>",
+                        "delimiter": ", ",
+                    },
+                    "packages": {"propType": "list", "format": "'<item>'", "delimiter": ", "},
+                },
             }
         }
     )
@@ -200,3 +184,21 @@ def test_add_platform_mapping():
         print(f"Error: {e}")
         print(f"Response: {response.json()}")
         assert response.status_code in {501, 409}
+
+
+def test_00X_drop_function_udo():
+    headers = {"Authorization": f"Bearer {TOKEN}"}
+
+    response: requests.Response = requests.delete(
+        "http://localhost:8181/api/opendic/v1/objects/function", headers=headers
+    )
+
+    try:
+        response.raise_for_status()
+        print(f"Status: {response.status_code}")
+        print(f"Response: {response.json()}")
+    except HTTPError as e:
+        print(f"Error: {e}")
+        print(f"Response: {response.json()}")
+
+    assert response.status_code in {200, 404}
