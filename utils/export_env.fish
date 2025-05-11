164d50d9882227ccb732f9eada6a80b832a3e989
akongstad <kongstad@outlook.com>
2025-05-11 20:39:05 +0200

    feat: add azure to benchmark compose file and add new secrets filepath

--- utils/export_env.fish
+++ utils/export_env.fish
@@ -1,9 +1,9 @@
-if test -f .env
-    for line in (cat .env | grep -v '^#' | grep -v '^\s*$')
+if test -f secrets/.env
+    for line in (cat secrets/.env | grep -v '^#' | grep -v '^\s*$')
         set item (string split -m 1 '=' $line)
         set -gx (string trim $item[1]) (string trim $item[2])
         echo "Exported $item[1]"
     end
 else
-    echo ".env file not found"
+    echo "secrets/.env file not found"
 end
